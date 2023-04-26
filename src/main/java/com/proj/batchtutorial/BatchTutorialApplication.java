7a3cda9423e925977b65f42d8c659816bc9c9d62
kang_jungwoo <kang_jungwoo@cocone.co.jp>
2023-04-26 17:00:37 +0900

    project upload

---
+++ src/main/java/com/proj/batchtutorial/BatchTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.proj.batchtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proj/batchtutorial/BatchTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.batchtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
